b862ee8f1c2556808b99d000f229f12bc58a55c1
andruha <molodoyelpil>
2021-05-24 12:12:21 +0300

    persik

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ketchup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
